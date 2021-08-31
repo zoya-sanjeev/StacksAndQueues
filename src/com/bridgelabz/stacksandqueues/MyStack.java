1e7aaf6fe48490ed62df62c5901bb0c964026743
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-31 23:50:03 +0530

    ADDED: implementation of push and printstack

--- src/com/bridgelabz/stacksandqueues/MyStack.java
+++ src/com/bridgelabz/stacksandqueues/MyStack.java
@@ -1,5 +1,17 @@
 package com.bridgelabz.stacksandqueues;
 
-public class MyStack {
-
+public class MyStack<K> {
+	LinkedList<K> linkedList;
+	
+	MyStack(){
+		this.linkedList=new LinkedList<>();
+	}
+	
+	void push(INode newNode) {
+		linkedList.add(newNode);
+	}
+	
+	void printStack() {
+		linkedList.printNodes();
+	}
 }
--- src/com/bridgelabz/stacksandqueues/StackQueueMain.java
+++ src/com/bridgelabz/stacksandqueues/StackQueueMain.java
@@ -1,5 +1,19 @@
 package com.bridgelabz.stacksandqueues;
 
+
 public class StackQueueMain {
+	public static void main(String[] args){
+		
+		Node<Integer> firstNode = new Node(56);
+		Node<Integer> secondNode = new Node(30);
+		Node<Integer> thirdNode = new Node(70);
+		
+		MyStack<Integer> myStack=new MyStack<>();
+		myStack.push(firstNode);
+		myStack.push(secondNode);
+		myStack.push(thirdNode);
+		myStack.printStack();
+		
+	}
 
 }
