7f3abde244084d7085010f80f129db4d54f3a754
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-31 23:24:28 +0530

    ADDED:Node Class

--- src/com/bridgelabz/stacksandqueues/Node.java
+++ src/com/bridgelabz/stacksandqueues/Node.java
@@ -1,5 +1,36 @@
 package com.bridgelabz.stacksandqueues;
 
+import com.bridgelabz.linkedlist.INode;
+
 public class Node {
+	
+	public class Node<K> implements INode<K>{
+		
+
+		private K key;
+		private INode next;
+		
+		public MyNode(K key){
+			this.key=key;
+			this.next=null;
+			
+		}
+		
+		@Override
+		public K getKey() {
+			return key;
+		}
+		@Override
+		public void setKey(K key) {
+			this.key = key;
+		}
+		@Override
+		public INode getNext() {
+			return next;
+		}
+		@Override
+		public void setNext(INode next) {
+			this.next = next;
+		}
 
 }
